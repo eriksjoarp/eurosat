4ce6e0a055573cff38e93dbb48c002e97a5c8401
eriksjoarp <erik.westring@gmail.com>
2022-10-23 07:18:10 +0200

    huge remake. now 2 default json files, dataset and swinv2 is used for default paramters. they are read into a dictionary and used in the swinv2 training class based on huggingface

--- classes_eurosat.py
+++ classes_eurosat.py
@@ -5,18 +5,27 @@ ToDo
 conf default for swin_config that is loaded and then changes can be made to it
 json_load config
 create base config json on disk
+
 class training
-class dataset
+class dataset from imagefolder
+class logging
+class eval
+
+clean up classes
+
+automate ablation
+
 
 
 '''
 import json
 import os,sys
 from ai_helper import constants_dataset as c_d
-from ai_helper import constants_ai_h
+from ai_helper import constants_ai_h as c_ai_h
 from ai_helper import dataset_load
 from ai_helper import dataset_load_helper
 from ai_helper import torch_help_functions
+from ai_helper import constants_training as c_t
 
 from helper import  erik_functions_files
 import eurosat_helper
@@ -53,180 +62,209 @@ from torchvision.transforms import (
 )
 
 
-
-
-
+'''
 class EurosatDataset:
     def __init__(self, conf_dataset_json):
         self.conf_dataset=conf_dataset_json  # json load from path
+'''
 
-
-
-# ToDo fix conf_json
 
 class SwinTraining:
-    def __init__(self, conf_json_path):
-        self.config_path=conf_json_path
-        self.config=[]
+    def __init__(self, config_json=None, config_json_path=None, config_json_dataset=None, config_json_dataset_path=None):     # start with reading the baseconfig and change it and save to disc before initializing this class
+        if config_json == None:
+            self.config_path=config_json_path
+            self.c = erik_functions_files.json_load(self.config_path)
+        else:
+            self.c = config_json
 
-        # check if resources are available, then book resources
-        # how should I do this, resource_config file?
-        # load config file defaults
-        ###############     params load from file later     ##################
+        if config_json_dataset == None:
+            self.config_path_dataset=config_json_dataset_path
+            self.c_d = erik_functions_files.json_load(self.config_path_dataset)
+        else:
+            self.c_d = config_json_dataset
 
-        self.EPOCHS = 1
-        self.lr = 2e-4
-        self.MODEL_CHECKPOINT = r'microsoft/swinv2-tiny-patch4-window8-256'
-        self.WEIGHT_DECAY = 0.0
+        self.initialize_config_values()
 
-        self.MODEL_NAME = self.MODEL_CHECKPOINT.split("/")[-1]
-        self.DIR_MODEL_CACHE = c_d.DIR_MODEL_CACHE
+        print(self.c)
+        print(self.c_d)
 
-        MODEL_ENDING = 'rgb-baseline_' + str(self.EPOCHS) + 'e_' + str(self.lr) + 'lr_' + str(self.WEIGHT_DECAY) + 'wd_'
-        self.MODEL_NAME_TRAINED = self.MODEL_NAME + '''-finetuned-eurosat-''' + MODEL_ENDING
+        # check if resources are available, then book resources, GPU
 
-        self.TRAIN_BATCH_SIZE = dataset_load_helper.get_batchsize(self.MODEL_NAME_TRAINED)
-        self.TRAIN_BATCH_SIZE = 16
-        self.EVAL_BATCH_SIZE = self.TRAIN_BATCH_SIZE
-        self.GRADIENT_ACCUMULATION_STEPS = int(160 / 2 / self.TRAIN_BATCH_SIZE)  # BATCH_SIZE * GPUs * GRAD_ACC = 128
-
-        self.DATALOADER_NUM_WORKERS = 4
-        self.WARMUP_RATIO = 0.25
-        self.PUSH_TO_HUB = False
-        self.TEST_SIZE = 0.1
-
-        self.DATASET_SMALL = True
-        self.DATA_DIR = c_d.DIR_DATASET_EUROSAT_RGB
-
-        self.DIR_TRAINING_DATA = os.path.join(constants_ai_h.DIR_EXPERIMENTS_SWIN, self.MODEL_NAME_TRAINED)
-        self.DIR_TRAINING_DATA = dataset_load_helper.create_data_dir(self.DIR_TRAINING_DATA)   # create unique directory
-        self.SAVE_DATA_DIR = dataset_load_helper.create_data_dir(self.DIR_TRAINING_DATA)
-
-        ######################################################################
 
         # check if gpus are available
         torch_help_functions.is_cuda_available()
 
         # extract features
-        self.feature_extractor = AutoFeatureExtractor.from_pretrained(self.MODEL_CHECKPOINT)
-        #self.normalize = Normalize(mean=self.feature_extractor.image_mean, std=self.feature_extractor.image_std)
-        self.normalize = self.normalize_create()
-        self.train_transforms = self.transforms_train_eurosat(self.feature_extractor)
-        self.val_transforms = self.transforms_val_eurosat()
+        self.feature_extractor = self.feature_extractor_swin()
 
         # load transformers
+        self.normalize = self.normalize_create()
+        self.train_transforms = self.transforms_train_eurosat()
+        self.val_transforms = self.transforms_val_eurosat()
 
         # load dataset
-        self.dataset_loaded = self.dataset_load()
+        self.datasets = self.dataset_load()
+
+        # map labels
+        self.label2id, self.id2label = self.map_labels()
 
         # preprocess datasets
-        self.dataset_loaded['train'].set_transform(self.preprocess_train)
-        self.dataset_loaded['test'].set_transform(self.preprocess_val)
-        self.dataset_loaded['val'].set_transform(self.preprocess_val)
+        self.datasets['train'].set_transform(self.preprocess_train)
+        self.datasets['test'].set_transform(self.preprocess_val)
+        self.datasets['val'].set_transform(self.preprocess_val)
 
         # load model
         self.model_swin = self.load_swin_model()
 
         # start training
         self.training_args_swin = self.training_args_get()
-        self.trainer_swin = self.trainer_get(self.training_args_swin)
+        self.trainer_swin = self.trainer_get()
+
+        #self.train_results = self.train()       # ToDo move this so the user chooses to do it
 
         # evaluate data
 
         # save metrics
+        #self.data_save()
 
         # release resources
 
 
+    # train the model
     def train(self):
-        self.train_results = self.trainer_swin.train()
+        _train_results = self.trainer_swin.train()
+        self._train_results = _train_results
+        return _train_results
+
 
     def eval(self):
         pass
 
-    def conf_matrix(self, y, y_pred, show_matrix=False):
+
+    # ToDo
+    # run this after changing the config to recalculate dependency values
+    def initialize_config_values(self):
+        self.c[c_t.S2_MODEL_NAME_TRAINED] = self.c[c_t.S2_MODEL_CHECKPOINT].split("/")[-1]    # remove / from checkpoint
+
+        if self.c[c_t.S2_MODEL_NAME_TRAINED_PRE_CHOSEN] == 'False':
+            _MODEL_ENDING = 'rgb-baseline_' + str(self.c[c_t.S2_TRAIN_ARGS_NUM_TRAIN_EPOCHS]) + 'e_' + str(self.c[c_t.S2_TRAIN_ARGS_LR]) + \
+                            'lr_' + str(self.c[c_t.S2_TRAIN_ARGS_WEIGHT_DECAY]) + 'wd'
+            self.c[c_t.S2_MODEL_NAME_TRAINED] = self.c[c_t.S2_MODEL_NAME] + '''-finetuned-eurosat-''' + _MODEL_ENDING
+        else:
+            self.c[c_t.S2_MODEL_NAME_TRAINED] = self.c[c_t.S2_MODEL_NAME_TRAINED_PRE_CHOSEN]
+
+        # retrieve batch sizes to use
+        self.c[c_t.S2_TRAIN_ARGS_PER_DEVICE_TRAIN_BATCH_SIZE] = dataset_load_helper.get_batchsize(self.c[c_t.S2_MODEL_NAME_TRAINED])
+        self.c[c_t.S2_TRAIN_ARGS_PER_DEVICE_EVAL_BATCH_SIZE] = self.c[c_t.S2_TRAIN_ARGS_PER_DEVICE_TRAIN_BATCH_SIZE]
+
+        self.c_d[c_t.DATASET_DIR_TRAINING_DATA_BASE] = os.path.join(c_ai_h.DIR_EXPERIMENTS_SWIN, self.c[c_t.S2_MODEL_NAME_TRAINED])
+
+        # ToDo probably wrong, what should it be? ERROR
+        #self.c_d[c_t.DATASET_DIR_TRAINING_DATA] = dataset_load_helper.create_data_dir(self.c_d[c_t.DATASET_DIR_TRAINING_DATA_BASE])           # create unique directory
+        self.c[c_t.S2_SAVE_DATA_DIR] = dataset_load_helper.create_data_dir(self.c_d[c_t.DATASET_DIR_TRAINING_DATA_BASE])
+
+        # validate the config
+        self.config_validate()
+
+
+    def config_validate(self):      # ToDo validate the config so it makes sense
+        pass
+
+    # extract features from the pretrained model
+    def feature_extractor_swin(self):
+        _feature_extractor = AutoFeatureExtractor.from_pretrained(self.c[c_t.S2_MODEL_CHECKPOINT])
+        return _feature_extractor
+
+    # calculate confusion matrix
+    def conf_matrix(self, y, y_pred, show_matrix=False, save_matrix=True):
         # y_pred = logreg.predict(X)  # Get the confusion matrix
         cf_matrix = confusion_matrix(y, y_pred)
         fig, ax = plt.subplots(figsize=(15, 10))
-        sns.heatmap(cf_matrix, linewidths=1, annot=True, ax=ax, fmt='g')
+        sns_plot = sns.heatmap(cf_matrix, linewidths=1, annot=True, ax=ax, fmt='g')
         if show_matrix:
             plt.show()
         else:
             print(cf_matrix)
-        save_filename = dataset_load_helper.get_filename_unique('figs', 'confusion_matrix_')
-        plt.imsave(save_filename)
+        if save_matrix:
+            _save_filename = dataset_load_helper.get_filename_unique('figs', 'confusion_matrix_')
+            plt.savefig(_save_filename)
 
-    def trainer_get(self, training_args):
-        trainer = Trainer(
-            self.model,
-            training_args,
-            train_dataset=self.dataset[c_d.DATASET_TRAIN],
-            eval_dataset=self.dataset[c_d.DATASET_VAL],
+
+    # define the huggingface trainer
+    def trainer_get(self):
+        _training_args = self.training_args_swin
+        _trainer_swin = Trainer(
+            self.model_swin,
+            _training_args,
+            train_dataset=self.datasets[c_d.DATASET_TRAIN],
+            eval_dataset=self.datasets[c_d.DATASET_VAL],
             tokenizer=self.feature_extractor,
             compute_metrics=self.compute_metrics_train,
             data_collator=self.collate_fn,
         )
-        return trainer
+        return _trainer_swin
 
     def load_swin_model(self):
         # load model
         # model = AutoModelForImageClassification.from_pretrained(
-        self.model = Swinv2ForImageClassification.from_pretrained(
-            self.MODEL_CHECKPOINT,
+        _model = Swinv2ForImageClassification.from_pretrained(
+            self.c[c_t.S2_MODEL_CHECKPOINT],
             label2id=self.label2id,
             id2label=self.id2label,
             # use_auth_token=,           # huggingface login
             ignore_mismatched_sizes=True,
             # provide this in case you're planning to fine-tune an already fine-tuned checkpoint
         )
-        return self.model
+        return _model
+
 
     def training_args_get(self):
         # config training
-        train_args = TrainingArguments(
+        _train_args = TrainingArguments(
             # f"{MODEL_NAME}-finetuned-eurosat",
-            self.SAVE_DATA_DIR,
+            self.c[c_t.S2_SAVE_DATA_DIR],
             remove_unused_columns=False,
-            evaluation_strategy="epoch",
-            save_strategy="epoch",
-            learning_rate=self.lr,
-            per_device_train_batch_size=self.TRAIN_BATCH_SIZE,
-            gradient_accumulation_steps=self.GRADIENT_ACCUMULATION_STEPS,
-            per_device_eval_batch_size=self.EVAL_BATCH_SIZE,
-            num_train_epochs=self.EPOCHS,
-            dataloader_num_workers=self.DATALOADER_NUM_WORKERS,
-            warmup_ratio=self.WARMUP_RATIO,
+            evaluation_strategy=self.c[c_t.S2_TRAIN_ARGS_EVALUATION_STRATEGY],
+            save_strategy=self.c[c_t.S2_TRAIN_ARGS_SAVE_STRATEGY],
+            learning_rate=self.c[c_t.S2_TRAIN_ARGS_LR],
+            per_device_train_batch_size=self.c[c_t.S2_TRAIN_ARGS_PER_DEVICE_TRAIN_BATCH_SIZE],
+            gradient_accumulation_steps=self.c[c_t.S2_TRAIN_ARGS_GRADIENT_ACCUMULATION_STEPS],
+            per_device_eval_batch_size=self.c[c_t.S2_TRAIN_ARGS_PER_DEVICE_EVAL_BATCH_SIZE],
+            num_train_epochs=self.c[c_t.S2_TRAIN_ARGS_NUM_TRAIN_EPOCHS],
+            dataloader_num_workers=self.c[c_t.S2_TRAIN_ARGS_DATALOADER_NUM_WORKERS],
+            warmup_ratio=self.c[c_t.S2_TRAIN_ARGS_WARMUP_RATIO],
             # weight_decay=WEIGHT_DECAY,
             logging_steps=10,
             load_best_model_at_end=True,
             metric_for_best_model="accuracy",
-            push_to_hub=self.PUSH_TO_HUB,
+            optim='adamw_torch',
+            push_to_hub=self.c[c_t.S2_TRAIN_ARGS_PUSH_TO_HUB],
             seed=1973,
             bf16=True,
             # gradient_checkpointing=True,       # super slow
             # auto_find_batch_size=True,         # is used with pip install accelerate
             # hub_token=,                        # when push to hub
         )
-        return train_args
+        return _train_args
 
-    def transforms_train_eurosat(self, feature_extractor):
-        train_transforms = Compose(
+    def transforms_train_eurosat(self):
+        _train_transforms = Compose(
             [
-                Resize(feature_extractor.size),
+                Resize(self.feature_extractor.size),
                 # Resize(320),
                 # RandomResizedCrop(feature_extractor.size),
                 # RandomResizedCrop(256),
                 # RandomHorizontalFlip(),
                 # RandAugment(),
-                CenterCrop(feature_extractor.size),
+                CenterCrop(self.feature_extractor.size),
                 ToTensor(),
                 self.normalize,
             ]
         )
-        return train_transforms
+        return _train_transforms
 
     def transforms_val_eurosat(self):
-        val_transforms = Compose(
+        _val_transforms = Compose(
             [
                 Resize(self.feature_extractor.size),
                 CenterCrop(self.feature_extractor.size),
@@ -234,14 +272,14 @@ class SwinTraining:
                 self.normalize,
             ]
         )
-        return val_transforms
+        return _val_transforms
 
     def compute_metrics_train(self, pred):
         labels = pred.label_ids
         preds = pred.predictions.argmax(-1)
         precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='weighted')
         acc = accuracy_score(labels, preds)
-        self.conf_matrix(labels, preds, show_matrix=False)
+        self.conf_matrix(labels, preds, show_matrix=False, save_matrix=True)
         return {
             'accuracy': acc,
             'f1': f1,
@@ -251,79 +289,94 @@ class SwinTraining:
 
     #  used to batch examples together. Each batch consists of 2 keys, namely pixel_values and labels
     def collate_fn(self, examples):
-        pixel_values = torch.stack([example["pixel_values"] for example in examples])
+        pixel_values = torch.stack([example['pixel_values'] for example in examples])
         labels = torch.tensor([example["label"] for example in examples])
-        return {"pixel_values": pixel_values, "labels": labels}
+        return {'pixel_values': pixel_values, 'labels': labels}
 
+    # preprocess training samples before the training by applying the train_transform
     def preprocess_train(self, example_batch):
         """Apply train_transforms across a batch."""
         example_batch["pixel_values"] = [
             self.train_transforms(image.convert("RGB")) for image in example_batch["image"]]  # examples["pixel_values"] = [image.convert("RGB").resize((100,100)) for image in examples["image"]]
         return example_batch
 
+    # preprocess training samples before the training by applying the eval_transform
     def preprocess_val(self, example_batch):
         """Apply val_transforms across a batch."""
         example_batch["pixel_values"] = [
             self.val_transforms(image.convert("RGB")) for image in example_batch["image"]]
         return example_batch
 
+    # calculate the normalized values to use as transform
     def normalize_create(self):
-        self.normalize = Normalize(mean=self.feature_extractor.image_mean, std=self.feature_extractor.image_std)
+        _normalize = Normalize(mean=self.feature_extractor.image_mean, std=self.feature_extractor.image_std)
+        return _normalize
 
-    # ToDo
+    '''
+    # ToDo load the json_config with training values
     def swin_json_load(self):
-        json.load(self.config['json'])
+        if erik_functions_files.json_load(self.config_path):
+            print('Fatal ERROR, cannot load json, aborting : ' + str(self.config_path))
+            exit(0)
+        _config = erik_functions_files.json_load(self.config_path)
+        return _config
 
-    # ToDo
+    # ToDo dump(save) the json_config with training values
     def swin_json_dump(self):
-        json.dump(self.config)
+        json.dump(self.c)
+    '''
 
+    # create the labelmapping between index and label_class
     def map_labels(self):
-        self.label2id, self.id2label = dict(), dict()
+        _label2id, _id2label = dict(), dict()
 
         classes = ['AnnualCrop', 'Forest', 'HerbaceousVegetation', 'Highway', 'Industrial', 'Pasture', 'PermanentCrop',
                    'Residential', 'River', 'SeaLake']
         for i, label in enumerate(classes):
-            self.label2id[label] = i
-            self.id2label[i] = label
+            _label2id[label] = i
+            _id2label[i] = label
+        return _label2id, _id2label
 
+    # load the dataset
     def dataset_load(self):
-        self.dataset = dataset_load.dataset_load_from_imagefolder(self.DATA_DIR, dataset_small=self.DATASET_SMALL)
+        _datasets = dataset_load.dataset_load_from_imagefolder(self.c_d[c_t.DATASET_PATH_DATASET], dataset_small=self.c_d[c_t.DATASET_SMALL])
+        # map labels
+        self.map_labels()
+        return _datasets
 
+    # load the metric used for training
     def metric_load(self, metric_list='accuracy'):
-        self.metric = load_metric(metric_list)
+        _metric = load_metric(metric_list)
+        return _metric
 
+    # dave data after training
     def data_save(self):
-        dir_save_model = os.path.join(c_d.DIR_MODELS_SAVED_SWIN, self.MODEL_NAME_TRAINED)
-        dir_eval = os.path.join(dir_save_model, 'eval')
+        _dir_save_model = os.path.join(c_d.DIR_MODELS_SAVED_SWIN, self.c[c_t.S2_MODEL_NAME_TRAINED])
+        _dir_eval = os.path.join(_dir_save_model, 'eval')
 
-        self.trainer.save_model(dir_save_model)
-        self.trainer.log_metrics(dir_save_model, metrics=self.train_results.metrics)
-        self.trainer.save_metrics(dir_save_model, metrics=self.train_results.metrics)
-        self.trainer.save_state()
+        self.trainer_swin.save_model(_dir_save_model)
+        self.trainer_swin.log_metrics(_dir_save_model, metrics=self._train_results.metrics)
+        self.trainer_swin.save_metrics(_dir_save_model, metrics=self._train_results.metrics)
+        self.trainer_swin.save_state()
 
-        metrics = self.trainer.evaluate()
-        self.trainer.log_metrics(dir_eval, metrics=metrics)
-        self.trainer.save_metrics(dir_eval, metrics=metrics)
+        metrics = self.trainer_swin.evaluate()
+        self.trainer_swin.log_metrics(_dir_eval, metrics=metrics)
+        self.trainer_swin.save_metrics(_dir_eval, metrics=metrics)
 
-        if self.PUSH_TO_HUB:
-            self.trainer.push_to_hub()
+        if self.c[c_t.S2_TRAIN_ARGS_PUSH_TO_HUB]:
+            self.trainer_swin.push_to_hub()
 
-        print('DIR_TRAINING_DATA  ' + self.DIR_TRAINING_DATA)
+        print('DIR_TRAINING_DATA  ' + self.c_d[c_t.DATASET_DIR_TRAINING_DATA])
 
-        print('dir_save_model  ' + dir_save_model)
-        print('dir_eval  ' + dir_eval)
+        print('dir_save_model  ' + _dir_save_model)
+        print('dir_eval  ' + _dir_eval)
 
-        self.training_args.do_train = False
+        self.training_args_swin.do_train = False
 
         # move confusion matrix pngs to traindir
-        dir_conf_matrix = os.path.join(dir_save_model, 'figs')
+        dir_conf_matrix = os.path.join(_dir_save_model, 'figs')
 
         # def copy_files_path(dir_from, dir_save, delete_old=False):
-        erik_functions_files.copy_files_from_dir_to_dir(constants_ai_h.DIR_LOCAL_FIGS, dir_conf_matrix,
+        erik_functions_files.copy_files_from_dir_to_dir(c_ai_h.DIR_LOCAL_FIGS, dir_conf_matrix,
                                                         delete_old_src=True, delete_old_save=True)
-
-
-
-
 
