44c83bee230efd19c89aae55f3324698109f5d45
eriksjoarp <erik.westring@gmail.com>
2022-10-28 02:00:05 +0200

    added json definitnions

--- eurosat_helper.py
+++ eurosat_helper.py
@@ -7,3 +7,18 @@ new dataset different resolution: 32,16
 class dataset_eurosat
 
 '''
+
+import os
+
+
+
+
+# ToDo
+def swin_json_load(path_config):
+    swin_config = 0
+    return swin_config
+
+# ToDo
+def swin_json_dump(config, path_config):
+    pass
+
